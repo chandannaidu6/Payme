6544965d1c721db7c360c20be9fc1b3b2602c15d
chandannaidu6 <chandannaidu0606@gmail.com>
2024-09-09 14:24:50 +0530

    added recent transactions

--- apps/user-app/app/(dashboard)/transfer/page.tsx
+++ apps/user-app/app/(dashboard)/transfer/page.tsx
@@ -4,6 +4,7 @@ import { BalanceCard } from "../../../components/BalanceCard";
 import { OnRampTransactions } from "../../../components/OnRampTransactions";
 import { getServerSession } from "next-auth";
 import { authOptions } from "../../lib/auth";
+import { Card } from "@repo/ui/card";
 async function getBalance(){
     const session = await getServerSession(authOptions);
     const balance = await prisma.balance.findFirst({
@@ -35,9 +36,34 @@ async function getOnRampTransactions(){
 
 }
 
+async function getRecent(){
+    const session = await getServerSession(authOptions)
+    if(!session?.user?.id){
+        throw new Error('User is not authenticated');
+    }
+    const p2p = await prisma.p2pTransfer.findMany({
+        where:{
+            OR:[
+                {fromUserId:Number(session?.user?.id)},
+                {toUserId:Number(session.user.id)},
+            
+        ]
+        },
+        orderBy:{
+            timestamp:'desc'
+        },
+        include:{
+            fromUser:true,
+            toUser:true
+        }
+
+    });
+    return p2p
+}
 export default async function() {
     const balance = await getBalance();
     const transactions = await getOnRampTransactions();
+    const p2p = await getRecent();
 
     return <div className="w-screen">
         <div className="text-4xl text-[#6a51a6] pt-8 mb-8 font-bold">
@@ -49,9 +75,29 @@ export default async function() {
             </div>
             <div>
                 <BalanceCard amount={balance.amount} locked={balance.locked} />
-                <div className="pt-4">
-                    <OnRampTransactions transactions={transactions} />
-                </div>
+                <Card title="Recent Transactions">
+                    <div>
+                        {p2p.map((transaction,index) => (
+                            <div 
+                                key={index}
+                                className='p-4 bg-gray-100 border rounded mb-4'>
+
+                                <div className='font-semibold text-lg'>
+                                    From:{transaction.fromUser.number ?? "Unknown User"}
+                                </div>
+                                <div className="text-sm">
+                                    To:{transaction.toUser.number ?? "Unknown User"}    
+                                </div> 
+                                <div className='text-sm'>
+                                    Amount:{transaction.amount}
+                                </div>   
+                                <div className="text-sm">
+                                    Date: {new Date(transaction.timestamp).toLocaleString()}
+                                 </div>    
+                            </div>
+                        ))}
+                    </div>
+                </Card>
             </div>
         </div>
     </div>
